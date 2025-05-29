bfbb5a32943eab550d0222c1277d5d7cd062187c
sander1946 <71840186+sander1946@users.noreply.github.com>
2025-05-29 19:00:20 +0200

    remove unused worker register

--- src/public/js/app.ts
+++ src/public/js/app.ts
@@ -1,2 +1,16 @@
+// TODO: check if this file is still needed, as spotify does not work offline yet
+// /**
+//  * Register the service worker for the app.
+//  * This will allow the app to work offline and cache assets.
+//  */
+// if ("serviceWorker" in navigator) {
+//   window.addEventListener("load", function() {
+//     navigator.serviceWorker
+//       .register("/public/js/serviceWorker.js")
+//       .then(res => console.log("service worker registered"))
+//       .catch(err => console.log("service worker not registered", err));
+//   });
+// }
+
   });
 }
